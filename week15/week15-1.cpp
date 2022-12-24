3fdfbc2403e35ea47e5397dcc3ff57f60a437870
hoodini2000 <114201429+hoodini2000@users.noreply.github.com>
2022-12-24 18:47:34 +0800

    Update week15-1.cpp

--- week15/week15-1.cpp
+++ week15/week15-1.cpp
@@ -1,1 +1,9 @@
-
+#include <stdio.h>
+int main()
+{
+    printf("Hello World\n");
+    char line[] = "Hello World";
+    printf("¾ã¼Æ %d\n", 100);
+    printf("¯BÂI¼Æ %f\n",3.141592653589793238462643383279);
+    printf("%s ¦r¦ê\n", line );
+}
