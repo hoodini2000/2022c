30d08e20617e7787a5f687149263c439d0355480
hoodini2000 <114201429+hoodini2000@users.noreply.github.com>
2022-12-24 12:42:28 +0800

    Update week15-2.cpp

--- week15/week15-2.cpp
+++ week15/week15-2.cpp
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 int main ()
 {
@@ -8,7 +7,7 @@ int main ()
     printf("%c : %d\n",'A','A');
     printf("%c : %d\n",'B','B');
     printf("%c : %d\n",'C','C');
-    printf("¤W­±¥Î¼Æ¦r 64 ¤Î³æ¤Ş¸¹'a'ªºµ²ªG³£¤@¼Ë\n");
+    printf("ä¸Šé¢ç”¨æ•¸å­— 64 åŠå–®å¼•è™Ÿ'a'çš„çµæœéƒ½ä¸€æ¨£\n");
     printf("%c :%d\n",97,97);
     printf("%c :%d\n",'a','a');
 }
